da2cbd77bd3fb39328b59f2946c4c4a75ab96c96
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-10-20 02:36:53 -0700

    add footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from './partials/Navigation';
 import Router from './Router';
+import Footer from './partials/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Router />
+        <div className="viewport-container">
+          <Navigation />
+          <Router />
+        </div>
+        <Footer />
       </div>
     );
   }
