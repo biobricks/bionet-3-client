eca48d62d32d406b8507cfd9d54b1b4f32122eb8
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-10-20 02:06:05 -0700

    add navigation component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navigation from './partials/Navigation';
 import Router from './Router';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <Router />
       </div>
     );
