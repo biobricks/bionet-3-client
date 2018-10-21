fa7755a221bc7a5c232122eaaf062077d7570653
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-10-20 20:26:16 -0700

    add membership requests

--- src/components/pages/LabList.jsx
+++ src/components/pages/LabList.jsx
@@ -23,6 +23,7 @@ class LabList extends Component {
       let labArray = res.data.data;
       let labsJoined = [];
       let labsNotJoined = [];
+      let labsRequestPending = [];
       let labsAll = [];
       for(let i = 0; i < labArray.length; i++){
         let lab = labArray[i];
@@ -34,13 +35,21 @@ class LabList extends Component {
             userExistsInLab = true;
           }
         }
-        if (!userExistsInLab) {
-          labsAll.push(lab);
+        let userRequestPending = false;
+        for(let k = 0; k < lab.joinRequests.length; k++){
+          let requesterId = lab.joinRequests[k]._id;
+          if (requesterId === userId){
+            userRequestPending = true;
+          }
+        }
+        if (!userExistsInLab && !userRequestPending) {
           labsNotJoined.push(lab);
+        } else if (!userExistsInLab && userRequestPending) {
+          labsRequestPending.push(lab);
         } else {
-          labsAll.push(lab);
           labsJoined.push(lab)
         }
+        labsAll.push(lab);         
       }
       this.setState({
         labs: labsAll,
@@ -71,17 +80,17 @@ class LabList extends Component {
       )
     });
     
-    // const labsNotJoined = this.state.labsNotJoined.map((lab, index) => {
-    //   return (
-    //     <Link 
-    //       key={shortid.generate()}
-    //       className="list-group-item list-group-item-action"
-    //       to={`/labs/${lab._id}`}
-    //     >
-    //       {lab.name}
-    //     </Link>
-    //   )
-    // });    
+    const labsNotJoined = this.state.labsNotJoined.map((lab, index) => {
+      return (
+        <Link 
+          key={shortid.generate()}
+          className="list-group-item list-group-item-action"
+          to={`/labs/${lab._id}`}
+        >
+          {lab.name}
+        </Link>
+      )
+    });    
 
     return (
       <div className="container-fluid">
@@ -98,13 +107,41 @@ class LabList extends Component {
                 <div className="card-header bg-dark text-light rounded-0">
                   <h4 className="card-title mb-0 text-capitalize">Labs Joined</h4>
                 </div>
+                {(this.state.labsJoined.length === 0) ? (
+                  <div className="card-body">
+                    <p className="card-text">
+                      You have not joined any Labs.
+                    </p>
+                  </div>                  
+                ) : null }
                 {(this.state.labsJoined.length > 0) ? (
                   <ul className="list-group list-group-flush">
                     {labsJoined}
                   </ul>
                 ) : null }
+
               </div>
             ) : null }
+            {(this.props.isLoggedIn) ? (
+              <div className="card mt-3 rounded-0">
+                <div className="card-header bg-dark text-light rounded-0">
+                  <h4 className="card-title mb-0 text-capitalize">Labs To Join</h4>
+                </div>
+                {(this.state.labsNotJoined.length === 0) ? (
+                  <div className="card-body">
+                    <p className="card-text">
+                      There are currently no Labs for you to Join.
+                    </p>
+                  </div>
+                ) : null }
+                {(this.state.labsNotJoined.length > 0) ? (
+                  <ul className="list-group list-group-flush">
+                    {labsNotJoined}
+                  </ul>
+                ) : null }
+              </div>
+            ) : null }
+
           </div>
         </div>
       </div>
--- src/components/pages/LabProfile.jsx
+++ src/components/pages/LabProfile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Auth from "../../modules/Auth";
 import { Link } from 'react-router-dom';
 import appConfig from '../../configuration.js';
 import axios from 'axios';
@@ -13,15 +14,12 @@ class LabProfile extends Component {
     this.state = {
       redirect: false,
       lab: {},
-      containers: [],
-      form: {
-        name: "",
-        description: "",
-        rows: 1, 
-        columns: 1
-      }
+      containers: []
     };
     this.getLab = this.getLab.bind(this);
+    this.onRequestLabMembership = this.onRequestLabMembership.bind(this);
+    this.onCancelRequestLabMembership = this.onCancelRequestLabMembership.bind(this);
+    this.updateLab = this.updateLab.bind(this);
   }
 
   getLab() {
@@ -38,12 +36,84 @@ class LabProfile extends Component {
     });    
   }
 
+  onRequestLabMembership(e) {
+    let lab = this.state.lab;
+    let users = [];
+    let joinRequests = [];
+    for(let i = 0; i < lab.users.length; i++){
+      let user = lab.users[i];
+      users.push(user._id);
+    };
+    for(let i = 0; i < lab.joinRequests.length; i++){
+      let request = lab.joinRequests[i];
+      joinRequests.push(request._id);
+    };
+    joinRequests.push(this.props.currentUser._id);
+    lab.users = users;
+    lab.joinRequests = joinRequests;
+    //console.log(lab);
+    this.updateLab(lab);
+  }
+
+  onCancelRequestLabMembership(e) {
+    let lab = this.state.lab;
+    let users = [];
+    let joinRequests = [];
+    for(let i = 0; i < lab.users.length; i++){
+      let user = lab.users[i];
+      users.push(user._id);
+    };
+    for(let i = 0; i < lab.joinRequests.length; i++){
+      let request = lab.joinRequests[i];
+      if (this.props.currentUser._id !== request._id){
+        joinRequests.push(request._id);
+      }
+    };
+    lab.users = users;
+    lab.joinRequests = joinRequests;
+    this.updateLab(lab);
+  }
+
+  updateLab(lab) {
+      let config = {
+        'headers': {  
+          'authorization': `Bearer ${Auth.getToken()}`
+        },
+        'json': true
+      };
+      let labId = this.props.match.params.labId;
+      axios.post(`${appConfig.apiBaseUrl}/labs/${labId}/membership`, lab, config)
+      .then(res => {     
+        //console.log(res.data.data);
+        this.getLab();
+      })
+      .catch(error => {
+        console.error(error);
+      });      
+   
+  }
+
   componentDidMount() {
     this.getLab();
   }  
 
   render() { 
     let users = this.state.lab.users || [];
+    let joinRequests = this.state.lab.joinRequests || [];
+    let currentUserIsMember = false;
+    let currentUserPendingApproval = false;
+    for(let i = 0; i < users.length; i++) {
+      let userId = users[i]._id || null;
+      if (userId === this.props.currentUser._id) {
+        currentUserIsMember = true;
+      }
+    }
+    for(let i = 0; i < joinRequests.length; i++) {
+      let requesterId = joinRequests[i]._id;
+      if (requesterId === this.props.currentUser._id) {
+        currentUserPendingApproval = true;
+      }
+    }
     const members = users.map((user, index) => {
       return (
         <Link 
@@ -77,58 +147,110 @@ class LabProfile extends Component {
 
               <div className="card rounded-0 mt-3">
                 <div className="card-header bg-dark text-light rounded-0">
-                  <h4 className="card-title mb-0">
-                    <i className="mdi mdi-teach mr-2" />
-                    {this.state.lab.name}
-                    <small className="float-right btn-toolbar" role="toolbar">
-                      <div className="btn-group" role="group">
-                        <Link 
-                          to={`/labs/${this.props.match.params.labId}/edit`}
-                          type="button" 
-                          className="btn btn-sm btn-primary"
-                        >
-                          <i className="mdi mdi-playlist-edit mr-1" />
-                          Edit
-                        </Link>
-                        <div className="btn-group" role="group">
-                          <button 
-                            id="add-button" 
-                            type="button" 
-                            className="btn btn-sm btn-success dropdown-toggle"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                          >
-                            <i className="mdi mdi-plus-box mr-1" />
-                            Add&nbsp;
-                          </button>
-                          <div
-                            className="dropdown-menu"
-                            aria-labelledby="add-button"
-                          >
-                            <Link 
-                              to={`/labs/${this.props.match.params.labId}/add/container`}
-                              className="dropdown-item"
+                  <div className="card-title mb-0">
+                    <i className="mdi mdi-xl mdi-teach" />
+                    <span>{this.state.lab.name}</span>
+                    <div className="float-right btn-toolbar" role="toolbar">
+                      <div className="btn-group border-0" role="group">
+                        {(currentUserIsMember) ? (
+                          <div className="btn-group" role="group">                           
+
+                            <button 
+                              id="add-button" 
+                              type="button" 
+                              className="btn btn-success dropdown-toggle rounded-0"
+                              data-toggle="dropdown"
+                              aria-haspopup="true"
+                              aria-expanded="false"
                             >
-                              <i className="mdi mdi-grid mr-2"/>
-                              Container
-                            </Link>
-                            <Link 
-                              to={`/labs/${this.props.match.params.labId}/add/physical`}
-                              className="dropdown-item"
+                              <i className="mdi mdi-lg mdi-plus-box mr-1" />
+                              Add&nbsp;
+                            </button>
+                            <div
+                              className="dropdown-menu"
+                              aria-labelledby="add-button"
                             >
-                              <i className="mdi mdi-flask mr-2"/>
-                              Physical
-                            </Link>
-                          </div>
-                        </div>  
-                        <button type="button" className="btn btn-sm btn-danger">
-                          <i className="mdi mdi-minus-box mr-1" />
-                          Remove
-                        </button>
+                              <Link 
+                                to={`/labs/${this.props.match.params.labId}/add/container`}
+                                className="dropdown-item"
+                              >
+                                <i className="mdi mdi-grid mr-2"/>
+                                Container
+                              </Link>
+                              <Link 
+                                to={`/labs/${this.props.match.params.labId}/add/physical`}
+                                className="dropdown-item"
+                              >
+                                <i className="mdi mdi-flask mr-2"/>
+                                Physical
+                              </Link>
+                            </div>
+
+                            <button 
+                              id="add-button" 
+                              type="button" 
+                              className="btn btn-primary dropdown-toggle rounded-0"
+                              data-toggle="dropdown"
+                              aria-haspopup="true"
+                              aria-expanded="false"
+                            >
+                              <i className="mdi mdi-lg mdi-settings-box mr-1" />
+                              Settings&nbsp;
+                            </button>
+                            <div
+                              className="dropdown-menu"
+                              aria-labelledby="add-button"
+                            >
+                              <Link 
+                                to={`/labs/${this.props.match.params.labId}/edit`}
+                                className="dropdown-item"
+                              >
+                                <i className="mdi mdi-grid mr-2"/>
+                                Edit
+                              </Link>
+                              <Link 
+                                to={`/labs/${this.props.match.params.labId}/remove`}
+                                className="dropdown-item"
+                              >
+                                <i className="mdi mdi-flask mr-2"/>
+                                Delete
+                              </Link>
+                            </div>
+
+                          </div> 
+                        ) : null }
+                        {(!currentUserIsMember && !currentUserPendingApproval) ? (
+                          <div className="btn-group" role="group">
+                            <button  
+                              className="btn btn-success rounded-0"
+                              onClick={this.onRequestLabMembership}
+                            >
+                              <i className="mdi mdi-account-plus mr-1" />
+                              Request Membership
+                            </button>
+
+                          </div> 
+                        ) : null }
+                        {(currentUserPendingApproval) ? (
+                          <div className="btn-group" role="group">
+                            <button 
+                              className="btn btn-warning rounded-0 disabled"
+                            >
+                              <i className="mdi mdi-account-plus mr-1" />
+                              Membership Pending Approval
+                            </button>                             
+                            <button 
+                              className="btn btn-sm btn-secondary rounded-0"
+                              onClick={this.onCancelRequestLabMembership}
+                            >
+                              <i className="mdi mdi-account-plus mr-1" />
+                              Cancel Request
+                            </button>
+                          </div> 
+                        ) : null }
                       </div>  
-                    </small>
-                  </h4>
+                    </div>
+                  </div>
                 </div>
                 <div className="card-body">
                   <p className="card-text">
