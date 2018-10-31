b0b0e81f49fcf39f3caf8d77f54aa18753094f00
unknown <ts22082@gmail.com>
2018-10-31 09:55:59 -0700

    create folder component style for Matt alert demo

---
+++ src/components/templateForMatt/templateAlert.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h3>lalala</h3>
+                <br/>
+                <p>bop bop bop</p>
+            </div>
+         );
+    }
+}
+
+export default Alert;
