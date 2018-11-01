d5085c281e1349e9237e89bddcce48583b4476b0
unknown <ts22082@gmail.com>
2018-10-31 17:30:00 -0700

    show another alert on on mount

--- src/components/pages/Toasty.jsx
+++ src/components/pages/Toasty.jsx
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 class Toasty extends Component {
   state = {  }
   componentDidMount() {
-    console.log('Wuttup')
     this.props.setAlert("success", `The thing was successfull.`);
     this.props.setAlert("error", `Noooooooooope.`);
+    this.props.setAlert("info", `This is informative.`);
+    this.props.setAlert("default", `This is informative.`);
   }
   render() { 
     return (
-
-      <div><h1>You've been toasted</h1></div>
+      <div>
+        <h1>You've been toasted</h1>
+      </div>
     );
   }
 }
