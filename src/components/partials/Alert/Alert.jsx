8ad8e896ce0be3c3f0537d5892e364d60d70c9ba
unknown <ts22082@gmail.com>
2018-10-31 17:31:29 -0700

    comment out alert

--- src/components/partials/Alert/Alert.jsx
+++ src/components/partials/Alert/Alert.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import { ToastContainer, Zoom } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './Alert.css';
+// import React, { Component } from 'react';
+// import { ToastContainer, Zoom } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import './Alert.css';
 
-class Alert extends Component {
+// class Alert extends Component {
   
-  constructor(props) {
-    super(props);
-    this.state = {
-      autoClose: 3000,
-      pauseOnFocusLoss: false,
-      draggable: false,
-      transition: null
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       autoClose: 3000,
+//       pauseOnFocusLoss: false,
+//       draggable: false,
+//       transition: null
+//     };
+//   }
 
-  componentDidMount() {
-    this.setState({
-      autoClose: this.props.autoClose || 3000,
-      pauseOnFocusLoss: this.props.pauseOnFocusLoss || true,
-      draggable: this.props.draggable || true,
-      transition: Zoom
-    });
-  }
+//   componentDidMount() {
+//     this.setState({
+//       autoClose: this.props.autoClose || 3000,
+//       pauseOnFocusLoss: this.props.pauseOnFocusLoss || true,
+//       draggable: this.props.draggable || true,
+//       transition: Zoom
+//     });
+//   }
 
-  render() {
-    return (
-      <ToastContainer {...this.state}/>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <ToastContainer {...this.state}/>
+//     );
+//   }
+// }
 
-export default Alert;  
+// export default Alert;  
