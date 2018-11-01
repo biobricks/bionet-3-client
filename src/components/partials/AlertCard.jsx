55f0dd9b365b8e3dc8caf844db059fe6c85bde25
unknown <ts22082@gmail.com>
2018-11-01 14:26:02 -0700

    fit to card

--- src/components/partials/AlertCard.jsx
+++ src/components/partials/AlertCard.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 
 class AlertCard extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: "Type",
+      message: "Message",
+    }
+  }
+
+  // setTypeAndMessage = () => {
+  //   this.setState({
+  //     type: this.props.alertType,
+  //     message: this.props.alertMessage
+  //   })
+  // }
+
+  componentDidMount() {
+    // this.setTypeAndMessage()
+    // console.log(this.props.alertType)
+    // console.log(this.props.alertMessage)
+  }
+
   render() {
     return (
       <div className="card mt-3">
         <div className="card-header bg-dark text-light">
-          <h4 className="card-title mb-0">{this.props.title}</h4>
+          <h4 className="card-title mb-0">{this.state.type}</h4>
         </div>
         <div className="card-body">
-          <p className="card-text">{this.props.message}</p>
+          <p className="card-text">{this.state.message}</p>
         </div> 
       </div>
     );
