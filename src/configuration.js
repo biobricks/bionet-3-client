9379e6464c6e7d95986147aa0a8ea12bd31c83e6
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-02-22 15:54:22 -0800

    add production endpoint

--- src/configuration.js
+++ src/configuration.js
@@ -1,4 +1,5 @@
 module.exports = {
-  "apiBaseUrl": "https://bionet.me/api/v1"
+  "apiBaseUrl": "https://endylab.stanford.edu/api/v1"
+  // "apiBaseUrl": "https://bionet.me/api/v1"
   //"apiBaseUrl": "http://localhost:3001/api/v1"
 };
