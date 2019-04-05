458dbdfd354dc4bd0ec2d74f286943bfceed8a6c
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-04-05 11:19:27 -0400

    update description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ class About extends Component {
                 <div className="card-body">
                   <h4>Open Source Biological Inventory Management</h4>
                   <p className="card-text">
-                    Welcome to BioNet. Keep track of your stuff, find what you need, and share as you like. The BioNet supports searching for biological material across multiple labs — all your inventory information is controlled locally by you. You decide if others can see what you wish to share. All BioNet software and associated materials are open source and free to use.
+                    Welcome to Bionet. Keep track of your stuff, find what you need, and share as you like. The Bionet supports searching for biological material across multiple labs — all your inventory information is controlled locally by you. You decide if others can see what you wish to share. All Bionet software and associated materials are open source and free to use.
                   </p>
                 </div>  
               </div>  
